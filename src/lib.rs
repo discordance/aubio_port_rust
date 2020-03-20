fc8cdf5e07c0d1a9ace407910d091a1f000cb70d
Aymeric Nunge <aymeric.nunge@gmail.com>
2020-03-20 18:30:57 +0100

    removed some allow

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,8 @@
 #![allow(dead_code)]
-#![allow(mutable_transmutes)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
-#![allow(unused_assignments)]
-#![allow(unused_mut)]
 #![feature(c_variadic)]
-#![feature(const_raw_ptr_to_usize_cast)]
-// #![feature(extern_types)]
-#![feature(main)]
-#![feature(ptr_wrapping_offset_from)]
-#![feature(register_tool)]
-#![register_tool(c2rust)]
 
 // import transpiled code
 mod transpiled;
